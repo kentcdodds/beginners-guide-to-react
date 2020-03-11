7ea77583bd395ce19759864c19c96f8f64b4a0c0
zac <zac@egghead.io>
2020-03-11 14:36:40 -0400

    Add folders to project

---
+++ 21-controlled-forms/src/index.js
@@ -0,0 +1,8 @@
+document.getElementById("app").innerHTML = `
+<h1>Hello Parcel!</h1>
+<div>
+  Look
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
+  for more info about Parcel.
+</div>
+`;
